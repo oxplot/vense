ae5c280ca10135b4c9f1f13582c6e0bb25c81be9
Mansour Behabadi <mansour@oxplot.com>
2019-03-22 23:29:24 +1100

    More tile

--- tile/tile.go
+++ tile/tile.go
@@ -27,7 +27,7 @@ func (g *Group) Add(tileName string) {
 		return
 	}
 	t := &tile{
-		index: len(g.tilesByName),
+		index: g.Size(),
 		name:  tileName,
 	}
 	g.tilesByName[tileName] = t
@@ -46,10 +46,14 @@ func (g *Group) EdgeSet(tile string, edge Edge) *Set {
 	}
 	e := t.edgeSets[edge]
 	if e == nil {
-		e = newSet(g)
+		e = NewSet(g)
 		t.edgeSets[edge] = e
 	}
 	return e
+}
+
+func (g *Group) Size() int {
+	return len(g.tilesByName)
 }
 
 type tile struct {
@@ -63,8 +67,8 @@ type Set struct {
 	group *Group
 }
 
-func newSet(g *Group) *Set {
-	return &Set{group: g}
+func NewSet(group *Group) *Set {
+	return &Set{group: group}
 }
 
 func (s *Set) Add(tile string) {
@@ -79,6 +83,22 @@ func (s *Set) Add(tile string) {
 		s.bits = b
 	}
 	s.bits[i/64] |= uint64(1 << uint(i%64))
+}
+
+func (s *Set) AddAll() {
+	gSize := s.group.Size()
+	if gSize == 0 {
+		return
+	}
+	s.bits = make([]uint64, (gSize-1)/64)
+	fullInts := gSize / 64
+	for i := 0; i < fullInts; i++ {
+		s.bits[i] = uint64(1<<64 - 1)
+	}
+	if gSize%64 == 0 {
+		return
+	}
+	s.bits[len(s.bits)-1] = (1<<uint(gSize%64) - 1)
 }
 
 func (s *Set) Remove(tile string) {
@@ -105,10 +125,43 @@ func (s *Set) Has(tile string) bool {
 	return s.bits[i/64]&uint64(1<<uint(i%64)) == 1
 }
 
+func (s *Set) Intersect(other *Set) {
+}
+
+func (s *Set) Union(other *Set) {
+}
+
+func (s *Set) Tiles() []string {
+	tiles := []string{}
+	for n := range s.group.tilesByName {
+		if s.Has(n) {
+			tiles = append(tiles, n)
+		}
+	}
+	return tiles
+}
+
 func (s *Set) Clear() {
 	s.bits = nil
 }
 
-type SetGrid struct {
-	cells [][]*tile
+func CollapseCell(grid [][]*Set, x, y int) {
+}
+
+func CollapseGrid(grid [][]*Set, startX, startY int) {
+}
+
+func GenerateGrid(width, height, randomSeed uint) (grid [][]*Set, resolved bool) {
+	grid = make([][]*Set, width)
+
+	for x := 0; x < width; x++ {
+		newCol := make([]*Set, height)
+		grid.cells[x] = newCol
+		for y := 0; y < height; y++ {
+			s := NewSet(group)
+			s.AddAll()
+			grid.cells[x][y] = s
+		}
+	}
+
 }
